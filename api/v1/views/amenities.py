72961c7d2d2eed4ff342ec59569fb236b6440a67
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-03-16 14:31:55 +0000

    amenities.py

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""
+Amenities view module.
+"""
+
+from flask import jsonify, abort, request
+from api.v1.views import app_views
+from models import storage, Amenity
+
+
+@app_views.route('/amenities', methods=['GET'])
+def get_amenities():
+    """Retrieves the list of all Amenity objects"""
+    amenities = [amenity.to_dict() for amenity
+                 in storage.all(Amenity).values()]
+    return jsonify(amenities)
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['GET'])
+def get_amenity(amenity_id):
+    """Retrieves a specific Amenity object"""
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity:
+        return jsonify(amenity.to_dict())
+    else:
+        abort(404)
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
+def delete_amenity(amenity_id):
+    """Deletes a specific Amenity object"""
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity:
+        storage.delete(amenity)
+        storage.save()
+        return jsonify({})
+    else:
+        abort(404)
+
+
+@app_views.route('/amenities', methods=['POST'])
+def create_amenity():
+    """Creates a new Amenity object"""
+    if not request.json:
+        abort(400, "Not a JSON")
+    if 'name' not in request.json:
+        abort(400, "Missing name")
+    amenity = Amenity(**request.json)
+    storage.new(amenity)
+    storage.save()
+    return jsonify(amenity.to_dict()), 201
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'])
+def update_amenity(amenity_id):
+    """Updates a specific Amenity object"""
+    amenity = storage.get(Amenity, amenity_id)
+    if not amenity:
+        abort(404)
+    if not request.json:
+        abort(400, "Not a JSON")
+    for key, value in request.json.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(amenity, key, value)
+    storage.save()
+    return jsonify(amenity.to_dict())
