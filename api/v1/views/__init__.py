1a0470918d49f5fc47ed572527d4b61ed8066612
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-03-17 11:04:37 +0000

    Updated file __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,3 +4,10 @@ from flask import Blueprint
 from api.v1.views.index import *
 
 app_views = Blueprint(url_prefix='/api/v1')
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.amenities import *
+from api.v1.views.cities import  # Add this line to import cities.py
+from api.v1.views.users import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places import *
