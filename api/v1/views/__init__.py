b118a6bb8768f6eb122747ce52b43369bcd9ac2e
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-03-17 11:20:12 +0000

    Updated pycodestyle cities.py and __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """import Blueprint function from flask module"""
 from flask import Blueprint
-from api.v1.views.index import *
+from api.v1.views.index import *  # Added line to import index.py
+from api.v1.views.states import *  # Added line to import states.py
+from api.v1.views.amenities import *  # Added line to import amenities.py
+from api.v1.views.cities import *  # Added line to import cities.py
+from api.v1.views.users import *  # Added line to import users.py
+from api.v1.views.places_reviews import *  # Line to import places_reviews.py
+from api.v1.views.places import *  # Added line to import places.py
 
 app_views = Blueprint(url_prefix='/api/v1')
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.amenities import *
-from api.v1.views.cities import  # Add this line to import cities.py
-from api.v1.views.users import *
-from api.v1.views.places_reviews import *
-from api.v1.views.places import *
