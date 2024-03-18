085eac56e28bd7b32d25ab71822319e6478d5726
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-03-18 09:49:21 +0000

    Changed back index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,7 +8,7 @@ from models import storage
 
 
 @app_views.route('/status')
-def api_views():
+def app_views():
     """Define a function named
         app_views that handles
         requests to the '/status'
